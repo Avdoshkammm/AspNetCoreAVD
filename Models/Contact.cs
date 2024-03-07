42604a7dc66ebb2e57eec695e190a291e019644e
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-03-08 00:54:50 +0300

    Валидация формы

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,11 +1,18 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Contact
     {
+        [Display(Name = "Введите имя")]
         public string Name { get; set; }
+        [Display(Name = "Введите фамилию")]
         public string Surname { get; set; }
+        [Display(Name = "Введите возраст")]
         public int Age { get; set; }
+        [Display(Name = "Введите почту")]
         public string Email { get; set; }
+        [Display(Name = "Введите сообщение")]
         public string Message { get; set; }
     }
 }
